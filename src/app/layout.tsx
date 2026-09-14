f3dbf6ad7316d94017df23a7f4ada9f83856a3ae
CheSubhro <mitsubhra@gmail.com>
2026-09-14 10:20:19 +0530

    layout Change

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,18 +4,9 @@ import type { Metadata } from "next";
 
 import "./globals.css";
 
-import { Geist } from "next/font/google";
-
-import { cn } from "@/lib/utils";
-
 import { Toaster } from "sonner";
 
 import AdminLayout from "@/components/layout/AdminLayout";
-
-const geist = Geist({
-  subsets: ["latin"],
-  variable: "--font-sans",
-});
 
 export const metadata: Metadata = {
   title: "PharmaTrix-Medical Shop Management System",
@@ -29,10 +20,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html
-      lang="en"
-      className={cn("font-sans", geist.variable)}
-    >
+    <html lang="en">
       <body>
         <AdminLayout>
           {children}
